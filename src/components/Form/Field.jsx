0ecad181ec7893f843cfb32901b1bc88549c0aaf
quannm97 <quan.nm.dever@gmail.com>
2023-06-29 16:11:25 +0700

    fix useEffect missing dependencies

--- src/components/Form/Field.jsx
+++ src/components/Form/Field.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
+import  { useEffect } from "react";
 import { ValidatedForm } from "./ValidatedForm";
 
-const Field = ({ title, name, rules, children }) => {
+const Field = (data) => {
+    const { title, name, rules, children } = data
     const validation = new ValidatedForm();
-    const onChange = validation.onChangeValue(name, rules);
-    useEffect(() => {
+    useEffect((name, rules,validation) => {
         validation.register(name, rules);
     }, []);
-
     const error = validation.errors[name];
+    const onChange = validation.onChangeValue(name, rules);
+
     console.log(validation.getError(name));
     return (
         <>
